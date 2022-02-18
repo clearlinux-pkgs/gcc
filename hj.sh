594ec8c67a53c2f595d37d7ff3cb46b8a2390ad8
Patrick McCarty <patrick.mccarty@intel.com>
2022-02-18 13:36:58 -0800

    Revise script for pulling HJ's branch
    - Change directory with git's `-C` option to avoid tracking the location
      of this git repo.
    - Use the `ours` option for the `ort` merge strategy to prefer HJ's
      branch to the upstream branch in case of a conflict. As of this
      writing, there is a minor conflict in a .texi file, and either
      ours/theirs options would suffice to resolve.
    - Avoid including merge commits in the `git shortlog` output, since the
      authorship will vary depending on the user running the script.
    
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- hj.sh
+++ hj.sh
@@ -1,10 +1,10 @@
-pushd ~/git/gcc
-git remote update
-git checkout hj3/users/hjl/x86/gcc-11
-git merge --no-edit origin/releases/gcc-11
-git shortlog origin/releases/gcc-11.. > ~/clear/packages/gcc/gcc-hj-latest.patch
-git diff origin/releases/gcc-11.. >> ~/clear/packages/gcc/gcc-hj-latest.patch
-git checkout master
-popd
-git add gcc-hj-latest.patch
+GCCGIT=~/git/gcc
 
+git -C $GCCGIT remote update
+git -C $GCCGIT checkout hj3/users/hjl/x86/gcc-11
+git -C $GCCGIT merge --no-edit -s ort -X ours origin/releases/gcc-11
+git -C $GCCGIT shortlog --no-merges origin/releases/gcc-11.. > ./gcc-hj-latest.patch
+git -C $GCCGIT diff origin/releases/gcc-11.. >> ./gcc-hj-latest.patch
+git -C $GCCGIT checkout master
+
+git add gcc-hj-latest.patch
